d04c9b9e2c831dfae6bee5bd73f07388ab0be63e
student-absc <129738325+student-absc@users.noreply.github.com>
2023-04-03 18:44:06 +0200

    New icons + Exit function

--- constants.ts
+++ constants.ts
@@ -1,32 +1,70 @@
 enum Icons {
-    ARROW = 0,
-    STOP = 1,
+    ARROW_IN = 0,
+    ARROW_OUT = 1,
+    STOP = 2,
 }
 
 let NUMBERS_IMAGES: { [key: number]: Image; } = {
     0: images.createImage(`
+. # # . .
+# . . # .
+# . . # .
+# . . # .
+. # # . .`),
+    1: images.createImage(`
+. # # . .
+. . # . .
+. . # . .
+. . # . .
+. . # . .`),
+    2: images.createImage(`
+. # # . .
+# . . # .
+. . # . .
+. # . . .
+# # # # .`),
+    3: images.createImage(`
+# # # # .
+. . . # .
 . # # # .
-# . . . #
-# . . . #
-# . . . #
-. # # # .`),
+. . . # .
+# # # # .`),
+    4: images.createImage(`
+# . . # .
+# . . # .
+# # # # .
+. . . # .
+. . . # .`),
+    5: images.createImage(`
+. # # # .
+# . . . .
+# # # # .
+. . . # .
+# # # # .`),
+    6: images.createImage(`
+# # # # .
+# . . . .
+# # # # .
+# . . # .
+# # # # .`),
     7: images.createImage(`
-# # # # #
+# # # # .
 . . . # .
 . . # . .
 . # . . .
 # . . . .`),
     8: images.createImage(`
-. # # # .
-# . . . #
-. # # # .
-# . . . #
-. # # # .`),
+# # # # .
+# . . # .
+. # # . .
+# . . # .
+# # # # .`),
 }
 
 let ICON_IMAGES: { [key: number]: Image; } = {
     0: images.arrowImage(ArrowNames.North),
-    1: images.createImage(`
+    1: images.arrowImage(ArrowNames.South),
+    2: images.createImage(`
 # . . . #
 . # . # .
 . . # . .
--- main.ts
+++ main.ts
@@ -4,43 +4,89 @@
 
 enum Mode {
     DISPLAY = 0,
-    ARROW = 1,
-    STOP = 2,
+    ENTER = 1,
+    EXIT = 2,
+    STOP = 3,
+}
+
+
+class Portal {
+    pin: AnalogInPin;
+
+    constructor(pin: AnalogInPin) {
+        this.pin = pin;
+    }
+
+    open(): void {
+        this.pin.digitalWrite(true);
+    }
+
+    close(): void {
+        this.pin.digitalWrite(false);
+    }
 }
 
 
 class PortalApp {
     places_left: number;
     mode: Mode;
+    portal: Portal;
 
     constructor() {
         this.places_left = 8;
         this.mode = Mode.DISPLAY;
+        this.portal = new Portal(pins.P0);
     }
 
-    main_loop() {
-        if (this.mode == Mode.DISPLAY) {
-            NUMBERS_IMAGES[this.places_left].showImage(0);
-        } else if (this.mode == Mode.ARROW) {
-            ICON_IMAGES[Icons.ARROW].showImage(0);
-        } else if (this.mode == Mode.STOP) {
-            ICON_IMAGES[Icons.STOP].showImage(0);
+    main_loop(): void {
+        try {
+            if (this.mode == Mode.DISPLAY) {
+                NUMBERS_IMAGES[this.places_left].showImage(0);
+            } else if (this.mode == Mode.ENTER) {
+                ICON_IMAGES[Icons.ARROW_IN].showImage(0);
+            } else if (this.mode == Mode.EXIT) {
+                ICON_IMAGES[Icons.ARROW_OUT].showImage(0);
+            } else if (this.mode == Mode.STOP) {
+                ICON_IMAGES[Icons.STOP].showImage(0);
+            }
+        } catch (exception) {
+            serial.writeLine(exception);
+            this.reset();
         }
     }
 
-    init() {
+    init(): void {
         basic.clearScreen();
         input.onButtonPressed(Button.A, (): void => this.enter());
+        input.onButtonPressed(Button.B, (): void => this.exit());
     }
 
-    enter() {
+    reset(): void {
+        this.places_left = 8;
+        this.mode = Mode.DISPLAY;
+    }
+
+    enter(): void {
         if (this.places_left > 0) {
             this.places_left -= 1;
-            this.mode = Mode.ARROW;
+            this.mode = Mode.ENTER;
+            this.portal.open();
         } else {
             this.mode = Mode.STOP;
         }
+        this.wait();
+    }
+
+    exit(): void {
+        this.places_left += 1;
+        this.mode = Mode.EXIT;
+        this.portal.open();
+        this.wait();
+    }
+
+    wait(): void {
         control.waitMicros(3000000);
+        this.portal.close();
         this.mode = Mode.DISPLAY;
     }
 }
